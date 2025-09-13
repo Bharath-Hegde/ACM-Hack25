bbf121425803fefb4ffd6e2b54f01909dffb667d
Bharath Hegde <bharath.hegde101@gmail.com>
2025-09-13 12:39:01 -0400

    remove add sample recipes button

--- plateful/src/pages/Recipes.jsx
+++ plateful/src/pages/Recipes.jsx
@@ -1,11 +1,8 @@
-import { Typography, Box, Fab, Button } from '@mui/material';
-import { Add, Refresh } from '@mui/icons-material';
+import { Typography, Box, Fab } from '@mui/material';
+import { Add } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import { useRecipes } from '../context/RecipeContext';
-import { collection, getDocs, addDoc, deleteDoc } from 'firebase/firestore';
-import { db } from '../firebase/config';
-import { sampleRecipes } from '../utils/recipeSchema';
 import RecipeList from '../components/RecipeList';
 import AddRecipeDialog from '../components/AddRecipeDialog';
 
@@ -43,30 +40,6 @@ const Recipes = () => {
     setAddDialogOpen(true);
   };
 
-  const handlePopulateSampleRecipes = async () => {
-    try {
-      // Clear existing recipes first
-      const recipesSnapshot = await getDocs(collection(db, 'recipes'));
-      const deletePromises = recipesSnapshot.docs.map(doc => deleteDoc(doc.ref));
-      await Promise.all(deletePromises);
-      
-      // Add sample recipes
-      const addedRecipes = [];
-      for (const recipe of sampleRecipes) {
-        // Remove the hardcoded ID before uploading to Firebase
-        const { id, ...recipeWithoutId } = recipe;
-        const docRef = await addDoc(collection(db, 'recipes'), recipeWithoutId);
-        addedRecipes.push({ id: docRef.id, ...recipeWithoutId });
-      }
-      
-      console.log('Sample recipes populated:', addedRecipes.length);
-      // Reload recipes - this will now be handled by the context's loadRecipes
-      // which will prevent concurrent calls
-      loadRecipes();
-    } catch (error) {
-      console.error('Error populating sample recipes:', error);
-    }
-  };
 
   const handleAddRecipeSubmit = async (recipeData) => {
     try {
@@ -84,15 +57,6 @@ const Recipes = () => {
         <Typography variant="h5" component="h1" sx={{ fontWeight: 'bold', color: 'text.primary' }}>
           Recipes
         </Typography>
-        <Button
-          variant="outlined"
-          startIcon={<Refresh />}
-          onClick={handlePopulateSampleRecipes}
-          size="small"
-          sx={{ mr: 1 }}
-        >
-          Add Sample Recipes
-        </Button>
       </Box>
 
       <RecipeList
